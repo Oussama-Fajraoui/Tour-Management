75e72e260d79d6612f949c093eacb656f1459881
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2023-03-01 14:06:57 +0100

    logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import {Container, Row, Button} from 'reactstrap';
+import { NavLink, Link } from 'react-router-dom';
+
+import logo from "../../assets/images/logo.png"
+
+const nav_links = [
+  {
+    path:'/home',
+    display:'Home',
+  },
+  {
+    path:'#',
+    display:'About',
+  },
+  {
+    path:'/tours',
+    display:'Tours',
+  },
+]
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <Container>
+        <Row>
+          <div className='nav_wrapper d-flex align-items-center justify-content-between'>
+            {/* {=====Logo=====} */}
+            <div className='logo'>
+              <img src={logo} alt=""/>
+            </div>
+            {/* {=====Logo END=====} */}
+          </div>
+        </Row>
+      </Container>
+    </header>
   )
 }
 
