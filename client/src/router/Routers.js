c1d034de3dadf860c838000c3f43a4d42bfdad30
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2023-03-01 11:10:20 +0100

    adding screens and navigation router links

---
+++ client/src/router/Routers.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import SearchResultList from '../pages/SearchResultList';
+import Tours from '../pages/Tours';
+import TourDetails from '../pages/TourDetails';
+
+
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Navigate to='/home' />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/searchresultlist' element={<SearchResultList />} />
+        <Route path='/tours' element={<Tours />} />
+        <Route path='/tour/:id' element={<TourDetails />} />
+    </Routes>
+  )
+}
+
+export default Routers
