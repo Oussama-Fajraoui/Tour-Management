325607cd515ce9dae61bd0fe1e2cf360a2e7ec9c
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2023-03-02 10:59:21 +0100

    Heaader

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import {Container, Row, Button} from 'reactstrap';
 import { NavLink, Link } from 'react-router-dom';
 
 import logo from "../../assets/images/logo.png"
+import './header.css';
 
 const nav_links = [
   {
@@ -24,7 +25,7 @@ const Header = () => {
     <header className="header">
       <Container>
         <Row>
-          <div className='nav_wrapper d-flex align-items-center justify-content-between'>
+          <div className='nav__wrapper d-flex align-items-center justify-content-between'>
             {/* {=====Logo=====} */}
             <div className='logo'>
               <img src={logo} alt=""/>
@@ -35,7 +36,7 @@ const Header = () => {
               <ul className="menu d-flex align-items-center gap-5">
                 {
                   nav_links.map((item,index) => (
-                    <li className='nav_item' key={index}>
+                    <li className='nav__item' key={index}>
                       <NavLink to={item.path}>{item.display}</NavLink>
                     </li>
                   ))
@@ -43,16 +44,17 @@ const Header = () => {
               </ul>
             </div>
             {/* {=====menu end=====} */}
-            <div className='nav_right d-flex align-items-center gap-4'>
-              <div className='nav_btns d-flex align-items-center gap-4'>
-                <Button className='btn secondary_btn'>
+            <div className='nav__right d-flex align-items-center gap-4'>
+              <div className='nav__btns d-flex align-items-center gap-4'>
+                <Button className='btn secondary__btn'>
                   <Link to="/login">Login</Link>
                 </Button>
-                <Button className='btn primary_btn'>
+                <Button className='btn primary__btn'>
                   <Link to="/register">Register</Link>
                 </Button>
               </div>
               <span className='mobile_menu'>
+              <i class="ri-menu-line"></i>
               </span>
 
             </div>
